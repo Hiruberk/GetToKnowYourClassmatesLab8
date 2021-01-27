e47a928d234f1bd63d81ae1262dc51261005fa49
Nate <nsdavis@mtu.edu>
2021-01-27 17:57:03 -0500

    Complete needs comments

--- Get To Know Your Classmates Lab 8/Program.cs
+++ Get To Know Your Classmates Lab 8/Program.cs
@@ -8,63 +8,97 @@ namespace Get_To_Know_Your_Classmates_Lab_8
     {
         static void Main(string[] args)
         {
-            List<string> firstNames = new List<string>() { "Ramon", "Antonio", "Joshua", "Nick", "Jeremiah", "Wendi", "Juliana", "Nathaniel", "Tommy", "Grace", "Jeffrey", "Josh", "Stephen" };
-            List<string> lastNames = new List<string>() { "Guarnes", "Manzari", "Carolin", "D'Oria", "Wyeth", "Magee", "Juliana", "Davis", "Waalkes", "Seymore", "Wohlfield", "Gallentine", "Jedlicki" };
-            List<string> hometown = new List<string>() { "Tigard, OR", "Beverly Hills, MI", "Novi, MI", "Canton, MI", "Crystal, MI", "Detroit, MI", "Denver, CO", "Berkley, MI", "Raleigh, NC", "Mesa, AZ", "Detroit, MI", "Baldwin, MI", "The Moon" };
-            List<string> favFood = new List<string>() { "Burgers", "Focaccia Barese", "Nalesniki", "Tacos", "Burgers", "Salami", "Tacos", "Steak", "Chicken Curry", "Sweet Potato Fries", "Steak", "Falafel", "Mooncakes" };
+            string[] firstNames = new string[] { "Ramon", "Antonio", "Joshua", "Nick", "Jeremiah", "Wendi", "Juliana", "Nathaniel", "Tommy", "Grace", "Jeffrey", "Josh", "Stephen" };
+            string[] lastNames = new string[] { "Guarnes", "Manzari", "Carolin", "D'Oria", "Wyeth", "Magee", "Juliana", "Davis", "Waalkes", "Seymore", "Wohlfield", "Gallentine", "Jedlicki" };
+            string[] hometown = new string[] { "Tigard, OR", "Beverly Hills, MI", "Novi, MI", "Canton, MI", "Crystal, MI", "Detroit, MI", "Denver, CO", "Berkley, MI", "Raleigh, NC", "Mesa, AZ", "Detroit, MI", "Baldwin, MI", "The Moon" };
+            string[] favFood = new string[] { "Burgers", "Focaccia Barese", "Nalesniki", "Tacos", "Burgers", "Salami", "Tacos", "Steak", "Chicken Curry", "Sweet Potato Fries", "Steak", "Falafel", "Mooncakes" };
             //count is 13 on all 4
 
-            Console.WriteLine("Welcome to Daytime Jan 2021 C# Class. Which student would you like to learn more about? (enter a number 1-14):");
-            
+            string home = "hometown";
+            string food = "favorite food";
+
+            Console.WriteLine("Welcome to Daytime Jan 2021 C# Class. Which student would you like to learn more about? (enter a number 1-13):");
+
+            string input = "";
 
             while (true)
             {
-           
+
                 try
                 {
-                    int input = int.Parse(Console.ReadLine());
-                    int index = input + 1;
+                    input = Console.ReadLine();
+                    int entry = GetIndex(input);
+                    int index = entry - 1;
 
-                    Console.WriteLine($"Student {index} is {firstNames[index]} {lastNames[index]}. What would you like to know about {firstNames[index]}? (enter hometown OR favorite food)");
+                    Console.WriteLine($"Student {entry} is {firstNames[index]} {lastNames[index]}. What would you like to know about {firstNames[index]}? (enter \"hometown\" or \"favorite food\")");
 
-                    do
+                    while(true)
                     {
-                        if (Console.ReadLine() != "hometown" || Console.ReadLine() != "favorite food")
-                        {
-                            Console.WriteLine("That data does not exist. Please try again. (enter a \"hometown\" OR \"favorite food\")");
-                        }
-                        else if (Console.ReadLine() == "hometown")
+                        string option = Console.ReadLine();
+                        if (home.Contains(option))
                         {
                             Console.WriteLine($"{firstNames[index]} is from {hometown[index]}.");
                             break;
                         }
-                        else
+                        else if (food.Contains(option))
                         {
+                           
                             Console.WriteLine($"{firstNames[index]} favorite food is {favFood[index]}.");
                             break;
                         }
-                    } while (true);
+                        else
+                        {
+                            Console.WriteLine("That data does not exist. Please try again. (enter a \"hometown\" or \"favorite food\")");
+                            
+                        }
+                    } 
                 }
                 catch (IndexOutOfRangeException)
                 {
-                    Console.WriteLine("Who is that? They must not be in our class. Try again. (enter a number 1-14)");
+                    Console.WriteLine("Who is that? They must not be in our class. Try again. (enter a number 1-13)");
+                    continue;
                 }
-                catch (FormatException)
-                {
-                    Console.WriteLine($"{Console.ReadLine()} was not a number at all. Please try again. (enter a \"hometown\" OR \"favorite food\")");
-                }
+
 
                 Console.WriteLine("would you like to know more? (y/n): ");
 
-                if(Console.ReadLine().ToLower() == "n")
+                if (Console.ReadLine().ToLower() == "n")
                 {
                     break;
                 }
 
-                Console.WriteLine("Which student would you like to learn more about ? (enter a number 1 - 14):");
+                Console.WriteLine("Which student would you like to learn more about ? (enter a number 1 - 13):");
             }
 
             Console.WriteLine("Thanks for stopping by!");
         }
+
+        public static int GetIndex(string message)
+        {
+            while (true)
+            {
+                string input = message;
+                try
+                {
+                    int numberInput = int.Parse(input);
+                    return numberInput;
+                    
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"{input} was not a number at all. Please try again. (enter a number 1 - 13)");
+                    input = Console.ReadLine();
+                    if (Int32.TryParse(input, out int numberInput))
+                    {
+                        return numberInput;
+                    }
+                    continue;
+                    
+                }
+                
+            }
+        }
     }
 }
+
